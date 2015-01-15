00a26d64dc860812e398cb9e99932063822b9af7
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-15 12:07:24 -0600

    Move 'rinruby' code to ruby_block (execution time) vs compile time. This prevents 'rinruby' from loading R before the execution of the r::default recipe that lays down the Rprofile.site file needed to prevent R from haning during package install waiting for CRAN mirror user input selection. [ISE-315]

--- recipes/rpackages.rb
+++ recipes/rpackages.rb
@@ -10,23 +10,24 @@
 include_recipe 'r'
 
 installed_r_packages = []
-begin
-	require "rinruby"
-	R.echo(enable=false)
-	R.eval "packages <- installed.packages()[,1]"
-	installed_r_packages = R.pull "packages"
-rescue Exception => e
-	# Ignore on compile phase of bootstrap/new-host when R isn't installed yet.
-	# Accept empty package list default.
-	unless e.message == "No such file or directory - R --interactive --slave"
-		raise e
-	end
+ruby_block "cache list of installed r packages" do
+  block do
+    require "rinruby"
+    R.echo(enable=false)
+    R.eval "packages <- installed.packages()[,1]"
+    installed_r_packages = R.pull "packages"
+  end
+  action :run
+end
+
+log "installed r packages" do
+  level :debug
+  message lazy {"Installed R packages: #{installed_r_packages}"}
 end
 
 node['r_nativex']['packages'].each do |pkg|
-  unless installed_r_packages.include?(pkg)
-    r_package pkg do
-      action :install
-    end
+  r_package pkg do
+    action :install
+    not_if { installed_r_packages.include?(pkg) }
   end
 end
